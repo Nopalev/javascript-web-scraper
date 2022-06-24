36b0d7c87d0299d2680964dc1ece2d46e847cb4a
Nopalev <faadhilah56@gmail.com>
2022-06-24 17:37:59 +0700

    First commit

---
+++ prakfinalScoreboardScraper.js
@@ -0,0 +1,88 @@
+import { load } from "cheerio";
+import fetch from 'node-fetch';
+import { writeFileSync } from "fs";
+
+// function to get the raw data
+const getRawData = (URL) => {
+   return fetch(URL)
+      .then((response) => response.text())
+      .then((data) => {
+         return data;
+      });
+};
+
+// URL for data
+const URL = "https://asdos.zydhan.com/scoreboard/final";
+
+// create csv file
+let csvContent = "";
+
+// start of the program
+const getPrakfinalList = async () => {
+   let scoreboard = [];
+   const prakfinalRawData = await getRawData(URL);
+
+   // parsing the data
+   const parsedPrakfinalData = load(prakfinalRawData);
+
+   // extracting the table data
+   const prakfinalDataTable = parsedPrakfinalData("table.Table_table__kzKHA")[0]
+      .children[1].children;
+
+   prakfinalDataTable.forEach((row) => {
+      // extracting `td` tags
+      if (row.name === "tr") { // note: the extracted data is saved into a javascript array of object
+          let team = null, problems = [], subProblem;
+
+        const columns = row.children.filter((column) => column.name === "td");
+
+        // extracting team
+        const teamColumn = columns[1];
+        if(teamColumn){
+            team = teamColumn.children[0];
+            if(team){
+                team = teamColumn.children[0].children[1];
+                if(team){
+                   team = teamColumn.children[0].children[1].children[0];
+                   if(team){
+                     team = teamColumn.children[0].children[1].children[0].children[0];
+                     if(team){
+                        team = teamColumn.children[0].children[1].children[0].children[0].data;
+                     }
+                   }
+                }
+            }
+        }
+        problems[0] = team;
+
+        // extracting problems
+        for(let i = 3; i<=26; i++){ // note that there are 24 problems form columns 3 to 26
+         const problemColumn = columns[i];
+         if(problemColumn){
+            subProblem = problemColumn.children[0];
+            if(subProblem){
+               subProblem = problemColumn.children[0].children[0];
+               if(subProblem){
+                  subProblem = problemColumn.children[0].children[0].data;
+                  if(subProblem === "-") subProblem = "30"; // if the team submitted to a problem but got no accepted
+                  else subProblem = "100"; // if the team got accepted
+               }
+            }
+            else subProblem = "0"; // if the team did not submit
+         }
+         problems[i-2] = subProblem;
+        }
+        
+        scoreboard.push(problems);
+      }
+   });
+   //console.log(scoreboard); // used to check scraping result
+   scoreboard.forEach(function(rowArray) { // formatting scrap result into 
+      let row = rowArray.join(",");
+      csvContent += row + "\r\n";
+  });
+  writeFileSync("scoreboard.csv", csvContent); // save the result to a csv file
+};
+
+// invoking the main function
+getPrakfinalList();
